7d45c2a65015ce109220b97467524c71dd3c89ff
MSDoshev <mihaildo@outlook.com>
2024-08-10 00:21:32 +0300

    Refactor Profile-details and create wishlist component

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -1,3 +1,5 @@
+import ProfileDetails from "@/components/profile/profile-details";
+import Wishlist from "@/components/profile/wishlist";
 import ListItem from "@/components/ui/ListItem";
 import UnorederedList from "@/components/ui/UnorderedList";
 
@@ -10,47 +12,12 @@ export default function ProfilePage() {
             <ListItem className="font-semibold">Profile</ListItem>
             <ListItem className="font-semibold">Wishlist</ListItem>
             <ListItem className="font-semibold">Purchase History</ListItem>
-            <ListItem className="font-semibold hover:bg-stone-100">Settings</ListItem>
+            <ListItem className="font-semibold hover:bg-stone-100">
+              Settings
+            </ListItem>
           </UnorederedList>
         </div>
-        <div className="p-10 w-[75%] max-w-3xl">
-          <h1 className="text-3xl font-bold text-center mb-5">Profile</h1>
-
-          <div className="flex flex-col items-center">
-            <img
-              src="/images/home/profile-placeholder.png"
-              alt="Profile"
-              className="w-24 h-24 rounded-full mb-4"
-            />
-            <h2 className="text-xl font-bold">Username</h2>
-            <p className="text-gray-600">user@example.com</p>
-
-            <div className="flex flex-col w-full mt-5 space-y-4">
-              <div className="flex justify-between">
-                <span className="font-bold">Full Name:</span>
-                <span>John Doe</span>
-              </div>
-              <div className="flex justify-between">
-                <span className="font-bold">Email:</span>
-                <span>user@example.com</span>
-              </div>
-              <div className="flex justify-between">
-                <span className="font-bold">Location:</span>
-                <span>Somelandia</span>
-              </div>
-              <div className="flex justify-between">
-                <span className="font-bold">Member Since:</span>
-                <span>January 2023</span>
-              </div>
-            </div>
-
-            <div className="mt-6">
-              <button className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-400">
-                Edit Profile
-              </button>
-            </div>
-          </div>
-        </div>
+        <Wishlist />
       </div>
     </div>
   );
--- src/components/ui/UnorderedList.js
+++ src/components/ui/UnorderedList.js
@@ -1,3 +1,7 @@
 export default function UnorederedList({ children }) {
-  return <ul className="flex flex-col gap-2 mt-5 w-[100%] justify-center items-center">{children}</ul>;
+  return (
+    <ul className="flex flex-col gap-2 mt-5 w-[100%] justify-center items-center">
+      {children}
+    </ul>
+  );
 }
