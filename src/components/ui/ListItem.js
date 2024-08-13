9a97e87ce231ab53575bba7ad03f02620da38056
MSDoshev <mihaildo@outlook.com>
2024-08-13 19:13:01 +0300

    Enhance ListItem component to support additional props

--- src/components/ui/ListItem.js
+++ src/components/ui/ListItem.js
@@ -1,7 +1,8 @@
-export default function ListItem({ children, className }) {
+export default function ListItem({ children, className, ...props }) {
   return (
     <li
       className={`p-2 hover:text-blue-500 hover:bg-stone-200 cursor-pointer text-lg ${className}`}
+    {...props}
     >
       {children}
     </li>
