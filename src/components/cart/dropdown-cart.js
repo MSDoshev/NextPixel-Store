18b0f5e7b1b7d155796ac579caf889cbd3a6c20d
MSDoshev <mihaildo@outlook.com>
2024-10-06 23:50:37 +0300

    Update header responsive design

--- src/components/cart/dropdown-cart.js
+++ src/components/cart/dropdown-cart.js
@@ -44,60 +44,92 @@ export default function CartDropdown() {
   };
 
   return (
-    <div className="relative group ">
-      <button className="hover:text-sky-500 flex items-center">
-        Cart ({totalItems})
-      </button>
-
-      <div className="absolute hidden group-hover:block bg-gray-800 text-white shadow-lg p-4 w-[350px] z-50 rounded-lg transition-all duration-300 ease-in-out transform group-hover:scale-100">
-        {cartItems.length > 0 ? (
-          <div className="max-h-72 overflow-y-auto scrollbar-thin scrollbar-thumb-dark scrollbar-track-gray-700">
-            {cartItems.map((item) => (
-              <div
-                key={item.id}
-                className="flex items-center justify-between py-3 px-2 bg-gray-700 hover:bg-gray-600 rounded-md mb-2 transition duration-200"
-              >
-                <img
-                  src={item.image}
-                  className="h-[60px] w-[45px] object-cover rounded-md"
-                  alt={item.name}
-                />
-                <div className="flex-1 mx-4">
-                  <h3 className="text-md font-semibold">{item.name}</h3>
-                  <p className="text-gray-400 text-sm">
-                    ${item.price} x {item.quantity}
-                  </p>
-                </div>
-                <div className="flex flex-col justify-center items-center">
-                  <p className="text-sm font-semibold text-sky-500">
-                    ${(item.price * item.quantity).toFixed(2)}
-                  </p>
-                  <button
-                    onClick={() => removeItemFromCart(item.id)}
-                    className="text-red-500 hover:text-red-600 font-bold ml-2"
-                  >
-                    Remove
-                  </button>
-                </div>
-              </div>
-            ))}
-            <div className="flex justify-between font-bold text-lg border-t border-gray-600 pt-3 mt-2">
-              <span>Total Price:</span>
-              <span>${totalPrice.toFixed(2)}</span>
-            </div>
-          </div>
-        ) : (
-          <p className="text-center text-gray-400">Your cart is empty</p>
-        )}
-
-        {cartItems.length > 0 && (
-          <Link
-            href="/checkout"
-            className="block text-center mt-4 bg-sky-500 text-white py-2 rounded-md hover:bg-sky-600 transition duration-200"
+    <div className="relative group">
+      <div className="md:hidden">
+        <Link href="/checkout" className="hover:text-sky-500 flex items-center">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="w-6 h-6 mr-1 hover:text-sky-500"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
           >
-            Checkout
-          </Link>
-        )}
+            <path d="M6.29977 5H21L19 12H7.37671M20 16H8L6 3H3M9 20C9 20.5523 8.55228 21 8 21C7.44772 21 7 20.5523 7 20C7 19.4477 7.44772 19 8 19C8.55228 19 9 19.4477 9 20ZM20 20C20 20.5523 19.5523 21 19 21C18.4477 21 18 20.5523 18 20C18 19.4477 18.4477 19 19 19C19.5523 19 20 19.4477 20 20Z" />
+          </svg>
+          ({totalItems})
+        </Link>
+      </div>
+
+      <div className="hidden md:block">
+        <Link href="/checkout" className="hover:text-sky-500 flex items-center">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="w-6 h-6 mr-1 hover:text-sky-500"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <path d="M6.29977 5H21L19 12H7.37671M20 16H8L6 3H3M9 20C9 20.5523 8.55228 21 8 21C7.44772 21 7 20.5523 7 20C7 19.4477 7.44772 19 8 19C8.55228 19 9 19.4477 9 20ZM20 20C20 20.5523 19.5523 21 19 21C18.4477 21 18 20.5523 18 20C18 19.4477 18.4477 19 19 19C19.5523 19 20 19.4477 20 20Z" />
+          </svg>
+          ({totalItems})
+        </Link>
+
+        <div className="absolute hidden group-hover:block bg-gray-800 text-white shadow-lg p-4 w-[350px] z-50 rounded-lg transition-all duration-300 ease-in-out transform group-hover:scale-100">
+          {cartItems.length > 0 ? (
+            <div className="max-h-72 overflow-y-auto scrollbar-thin scrollbar-thumb-dark scrollbar-track-gray-700">
+              {cartItems.map((item) => (
+                <div
+                  key={item.id}
+                  className="flex items-center justify-between py-3 px-2 bg-gray-700 hover:bg-gray-600 rounded-md mb-2 transition duration-200"
+                >
+                  <img
+                    src={item.image}
+                    className="h-[60px] w-[45px] object-cover rounded-md"
+                    alt={item.name}
+                  />
+                  <div className="flex-1 mx-4">
+                    <h3 className="text-md font-semibold">{item.name}</h3>
+                    <p className="text-gray-400 text-sm">
+                      ${item.price} x {item.quantity}
+                    </p>
+                  </div>
+                  <div className="flex flex-col justify-center items-center">
+                    <p className="text-sm font-semibold text-sky-500">
+                      ${(item.price * item.quantity).toFixed(2)}
+                    </p>
+                    <button
+                      onClick={() => removeItemFromCart(item.id)}
+                      className="text-red-500 hover:text-red-600 font-bold ml-2"
+                    >
+                      Remove
+                    </button>
+                  </div>
+                </div>
+              ))}
+              <div className="flex justify-between font-bold text-lg border-t border-gray-600 pt-3 mt-2">
+                <span>Total Price:</span>
+                <span>${totalPrice.toFixed(2)}</span>
+              </div>
+            </div>
+          ) : (
+            <p className="text-center text-gray-400">Your cart is empty</p>
+          )}
+
+          {cartItems.length > 0 && (
+            <Link
+              href="/checkout"
+              className="block text-center mt-4 bg-sky-500 text-white py-2 rounded-md hover:bg-sky-600 transition duration-200"
+            >
+              Checkout
+            </Link>
+          )}
+        </div>
       </div>
     </div>
   );
--- src/components/header/main-header.js
+++ src/components/header/main-header.js
@@ -22,41 +22,35 @@ export default async function MainHeader() {
   }
 
   return (
-    <header className="bg-stone-900 text-white flex items-center justify-between h-20 w-full px-4 md:px-8  sticky top-0 z-50">
+    <header className="bg-stone-900 text-white flex items-center justify-between h-20 w-full px-4 md:px-8 sticky top-0 z-50">
       <div id="logo">
         <Link href="/" className="hover:text-sky-500 text-2xl">
           NextPixel
         </Link>
       </div>
 
-      {/* Hamburger menu (visible on small screens) */}
+      <div className="flex justify-center items-center lg:hidden pl-[100px]">
+        <CartDropdown />
+      </div>
       <input type="checkbox" id="menu-toggle" className="hidden peer" />
-      <label htmlFor="menu-toggle" className="block md:hidden cursor-pointer">
+      <label htmlFor="menu-toggle" className="block lg:hidden cursor-pointer">
         <div className="w-8 h-1 bg-white mb-1"></div>
         <div className="w-8 h-1 bg-white mb-1"></div>
         <div className="w-8 h-1 bg-white"></div>
       </label>
 
-      {/* Full menu for large screens */}
-      <nav className="hidden md:flex space-x-8">
-        <Link href="/news" className="p-4 pb-7 border-b-white hover:border-b-2">
+      <nav className="hidden lg:flex space-x-8 pl-[260px]">
+        <Link href="/news" className="p-4 border-b-white hover:border-b-2">
           News
         </Link>
-        <Link
-          href="/store"
-          className="p-4 pb-7 border-b-white hover:border-b-2"
-        >
+        <Link href="/store" className="p-4 border-b-white hover:border-b-2">
           Store
         </Link>
-        <Link
-          href="/contact"
-          className="p-4 pb-7 border-b-white hover:border-b-2"
-        >
+        <Link href="/contact" className="p-4 border-b-white hover:border-b-2">
           Contact Us
         </Link>
       </nav>
 
-      {/* Mobile dropdown menu */}
       <nav className="absolute left-0 top-full bg-stone-900 w-full flex-col items-center space-y-4 py-4 text-center hidden peer-checked:flex z-50 lg:hidden">
         <Link href="/news" className="block text-white hover:text-sky-500">
           News
@@ -67,14 +61,10 @@ export default async function MainHeader() {
         <Link href="/contact" className="block text-white hover:text-sky-500">
           Contact Us
         </Link>
-        <div className="flex flex-col gap-4 items-center">
-          <CartDropdown />
-          {authOptions}
-        </div>
+        {authOptions}
       </nav>
 
-      {/* Cart and Auth options */}
-      <div className="hidden md:flex gap-4 items-center">
+      <div className="hidden lg:flex space-x-[150px] items-center">
         <CartDropdown />
         {authOptions}
       </div>
